b22cdf1d3ee416da79733a3e37d655eb828db208
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-01 19:09:37 -0300

    resolução dos 5 primeiros exercícios

--- semana4/funcoes/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -10,4 +10,54 @@
 
 // Exercício 3
 
+    // Caso X seja divísel por 2 sem que haja sobra número, o código multiplicará ele por ele mesmo e retornará o resultado. 
+
+// Exercício 4 
+
     // a)
+    function sobreMim() {
+    console.log("Eu sou Milena, tenho 22 anos, moro em Sâo Paulo e sou estudante")
+    }
+
+    sobreMim()
+
+    // b)
+    let nome = prompt ("Qual o seu nome?")
+    let idade = prompt ("Qual a sua idade?")
+    let endereco = prompt ("Em qual cidade você mora?")
+    let ocupacao = confirm("Você é estudante? Tecle 'ok' para sim e 'cancelar' para não")
+
+    function sobreMim() {
+    {
+    if (ocupacao = true) {
+        return "sou"
+    } else {
+        return "não sou"
+    }
+    }
+    }
+
+    console.log("Eu sou ", nome, " tenho ", idade, " anos ", " moro em ",  endereco, " e sou ", ocupacao)
+
+// Exercício 5 
+    // a) 
+    function soma (x, y) {
+        let soma = x + y
+        return soma
+      }
+      
+      soma (1, 2)   
+
+    // b) 
+    function coletarNumeros (n1, n2) {
+        if (n1 >= n2) {
+            console.log(n1, " é maior ou igual a ", n2)
+        }
+        else {
+            console.log(n1, "é menor que ", n2)
+        }
+    }
+
+    coletarNumeros (1, 2)
+
+
