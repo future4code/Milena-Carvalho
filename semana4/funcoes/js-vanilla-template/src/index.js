b95a3c62adee33a990d9ba15f240a977cbdde746
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-01 17:03:49 -0300

    Exercícios aula 14

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,13 @@
+// Exercício 1
+
+    // a) Serão impressos os números 10 e 50. 
+    // b) O sistema não identificaria a função e não imprimirá nada. 
+
+// Exercício 2
+
+    // a) O sistema imprimirá os nomes Darvas e Caio. 
+    // b) Se o valor da primeira linha for alterado para ["Amanda", "Caio"], o sistema imprimirá ambos os nomes. Se alterar o parâmetro (arrayDeNomes) para ("Amanda", "Caio"), o console imprimirá as letras A e M.
+
+// Exercício 3
+
+    // a)
