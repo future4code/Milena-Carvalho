3aaab79540f5178b6691c1a1a9ac568b1d0bed31
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-03 17:05:05 -0300

    início exercício objetos

---
+++ semana4/objetos/blog-boilerplate/index.js
@@ -0,0 +1,6 @@
+let conteudoPost = {
+    titulo: document.getElementById("titulo-post").value,
+    autor: document.getElementById("autor-post").value,
+    conteudo: document.getElementById("conteudo-post").value,
+
+}
