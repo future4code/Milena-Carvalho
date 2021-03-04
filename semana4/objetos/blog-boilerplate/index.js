be487a4971b6019a6e23b9e366fff6c91c0a37e1
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-03 22:12:03 -0300

    Continuação exercício

--- semana4/objetos/blog-boilerplate/index.js
+++ semana4/objetos/blog-boilerplate/index.js
@@ -1,6 +1,28 @@
-let conteudoPost = {
-    titulo: document.getElementById("titulo-post").value,
-    autor: document.getElementById("autor-post").value,
-    conteudo: document.getElementById("conteudo-post").value,
+function criarPost () {
 
+    let conteudoPost = {
+        titulo: document.getElementById("titulo-post").value,
+        autor: document.getElementById("autor-post").value,
+        conteudo: document.getElementById("conteudo-post").value,
+    }
+    
+    let posts = []
+
+    posts.push(conteudoPost)
+
+    document.getElementById("titulo-post").value = ""
+
+    document.getElementById("autor-post").value = ""
+
+    document.getElementById("conteudo-post").value = ""
+
+    inserirPosts(posts)
+}
+
+function inserirPosts (posts) {
+    let elemento=document.createElement("div")
+
+    elemento.appendChild(document.createTextNode(posts[0].titulo))
+
+    document.getElementById("posts").appendChild(elemento)
 }
