d9f6b6c38a56f39eb323c3a92be62fb07a282878
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-10 17:39:03 -0300

    Fotos diferentes de perfil e feed

--- semana5/componentes-de-classe/insta4 2/src/App.js
+++ semana5/componentes-de-classe/insta4 2/src/App.js
@@ -8,20 +8,20 @@ class App extends React.Component {
       <div className={'app-container'}>
         <Post
           nomeUsuario={'Juliette'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+          fotoUsuario={'https://picsum.photos/50/50?random=1'}
+          fotoPost={'https://picsum.photos/200/150?random=1'}
         />
 
         <Post
           nomeUsuario={'Camilla'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+          fotoUsuario={'https://picsum.photos/50/50?random=2'}
+          fotoPost={'https://picsum.photos/200/150?random=2'}
         />
 
         <Post
           nomeUsuario={'João'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+          fotoUsuario={'https://picsum.photos/50/50?random=3'}
+          fotoPost={'https://picsum.photos/200/150?random=3'}
         />  
       </div>
     );
